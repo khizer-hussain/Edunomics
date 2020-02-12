a0b171db2c2c9b5bbf91052e647526cb9d228851
Khizer Hussain <makh9952@gmail.com>
2020-02-12 14:50:09 +0530

    Updates

---
+++ precache-manifest.7e7b074e29e25305863f1175c66bcfce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad467dbc560b9d3d9bdf515faf021ed9",
+    "url": "/edunomics/index.html"
+  },
+  {
+    "revision": "93e94afc69d47d540915",
+    "url": "/edunomics/static/css/main.b93e5a4d.chunk.css"
+  },
+  {
+    "revision": "33feba5c3c8da07703c1",
+    "url": "/edunomics/static/js/2.2ea87118.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/edunomics/static/js/2.2ea87118.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "93e94afc69d47d540915",
+    "url": "/edunomics/static/js/main.8471fed7.chunk.js"
+  },
+  {
+    "revision": "ea39fb275c23d6b1d1fe",
+    "url": "/edunomics/static/js/runtime-main.962d1471.js"
+  }
+]);
